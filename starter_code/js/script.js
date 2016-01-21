21bf5644fd3bc50d09faccfdcdf31f90cf3835d2
Sabiha <sabihasultana@mybihs.org>
2016-01-21 22:59:20 +0000

    flickr Search

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,1 +1,21 @@
 // YOUR CODE GOES HERE
+$(document).ready(function(){
+    
+    function action(APIresult){
+        console.log(APIresult);
+        $.each(APIresult, function(i,item){
+            var storeLink = item.media.m;
+            console.log(storeLink);
+            var image = "<img src=" + storeLink + " height='200' width='200'>"
+            $('#search-results').append(image);
+        });
+    }
+    
+    $('#search-btn').click(function(){
+        var searchItem = $('#search-query').val();
+        console.log(searchItem);
+        
+        searchFlickr(searchItem, action);
+    });
+    
+});
